eb578ce2c4ac98f3f224e3420ddc64ad7b9d9972
Kartik Kannapur <kartikkannapur@gmail.com>
2018-03-09 18:40:02 -0500

    Python Libraries - Spark - Added

--- Python_Libraries.py
+++ Python_Libraries.py
@@ -22,3 +22,14 @@ from sklearn.model_selection import cross_val_score
 # #X_trainval, X_test, y_trainval, y_test = train_test_split(X, y)
 # #X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval)
 from sklearn.model_selection import GridSearchCV
+
+# #Spark
+import os
+import sys
+spark_home = os.environ.get('SPARK_HOME', None)
+
+if not spark_home:
+    raise ValueError('SPARK_HOME environment variable is not set')
+
+sys.path.insert(0, os.path.join(spark_home, 'python'))
+sys.path.insert(0, os.path.join(spark_home, 'C:/Users/karti/Spark/spark-2.3.0-bin-hadoop2.7/python/lib/py4j-0.10.6-src.zip'))
